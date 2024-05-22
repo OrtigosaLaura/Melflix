ebaa3c10f9547a067bfb3264b93c8e71434f8211
OrtigosaLaura <lauraortigosa378@gmail.com>
2024-05-22 14:46:33 -0300

    finalização da camada de dados

--- Melflix/Models/Movie.cs
+++ Melflix/Models/Movie.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace Melflix.Models;
 
@@ -23,12 +24,33 @@ namespace Melflix.Models;
        [StringLength(8000, ErrorMessage = "O Resumo deve possuir no máximo 8000")]
 
        public string Synopsis { get; set; }
+
        [Column(TypeName = "Year")]
+       [Display(Name = "Ano da Estrela")]
        public Int16 MovieYear { get; set; }
+
+       [Display(Name = "Duração (em minutos)")]
+       [Required(ErrorMessage = "Por favor, informe a duração em minutos")]
 
        public Int16 Duration { get; set; }
 
-       public byte AgeRating { get; set; }
+       [Display(Name = "Classificação Etária")]
+       [Required(ErrorMessage ="Por favor, informe a classificação etária")]
+
+       public byte AgeRating { get; set; } = 0;
+
+       [Display(Name = "Foto")]
+       [StringLength(200)]
 
        public string Image { get; set; }
+
+       [NotMapped]
+       [Display(Name = "Duração")]
+
+       public string HourDuration {get {
+           return TimeSpan.FromMinutes(Duration).ToString(@"h'h 'm'min'");
+       } }
+
+       public ICollection<MovieGenre> Genres { get; set; }
+
     }
